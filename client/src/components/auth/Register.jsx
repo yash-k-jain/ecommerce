cd4a51579ccc4374cbcb7d40b37bdd0eaff068d0
Yash <jainy092005@gmail.com>
2024-08-27 22:55:17 +0530

    error while integrating stepper

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -8,70 +8,23 @@ import { useMutation } from "react-query";
 
 import { toast } from "react-hot-toast";
 
-const Register = () => {
+const Register = ({ user: formData, setUser: setFormData, isLoading }) => {
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    name: "",
-    phone: "",
-  });
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const {
-    mutate: register,
-    isLoading,
-  } = useMutation({
-    mutationFn: async () => {
-      try {
-        const res = await fetch("/api/auth/register", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        });
-
-        const data = await res.json();
-        if (!res.ok) {
-          throw new Error(data.error || "Failed to register");
-        }
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
-    },
-
-    onSuccess: () => {
-      toast.success("Registered successfully");
-      setFormData({
-        email: "",
-        password: "",
-        name: "",
-        phone: "",
-      })
-      navigate("/");
-      Cookies.set("isRegistered", true);
-    },
-
-    onError: (error) => {
-      toast.error(error.message);
-    },
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    register();
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   register();
+  // };
 
   useEffect(() => {
-    if(Cookies.get("isRegistered") === "true") {
+    if (Cookies.get("isRegistered") === "true") {
       navigate("/");
     }
-  }, [])
+  }, []);
 
   return (
     <Box
@@ -88,7 +41,7 @@ const Register = () => {
       >
         Register
       </Typography>
-      <form onSubmit={handleSubmit} style={{ width: "100%" }}>  
+      <form style={{ width: "100%" }}>
         <FormControl fullWidth sx={{ mt: 2 }}>
           <Input
             id="email"
@@ -135,9 +88,9 @@ const Register = () => {
             onChange={handleChange}
           />
         </FormControl>
-        <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+        {/* <Button type="submit" variant="contained" sx={{ mt: 2 }}>
           {isLoading ? "Loading..." : "Register"}
-        </Button>
+        </Button> */}
       </form>
     </Box>
   );
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,11 +1,67 @@
 import React from "react";
 import Register from "../components/auth/Register";
 import Login from "../components/auth/Login";
+
 import { useParams } from "react-router-dom";
 
+import { useMutation } from 'react-query'
+import Address from "../components/auth/Address";
+import StepperComponent from "../components/common/Stepper";
+
 const Auth = () => {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    name: "",
+    phone: "",
+    line1: "",
+    postal: "",
+    city: "",
+    state: "",
+    country: "",
+  });
+
+  const { mutate: register, isLoading } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch("/api/auth/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Failed to register");
+        }
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+
+    onSuccess: () => {
+      toast.success("Registered successfully");
+      setFormData({
+        ...formData,
+        email: "",
+        password: "",
+        name: "",
+        phone: "",
+      });
+      navigate("/");
+      Cookies.set("isRegistered", true);
+    },
+
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
   const { type } = useParams();
-  return <>{type === "register" ? <Register /> : <Login />}</>;
+  return <>{type === "register" ? <Register user={user} setUser={setUser} register={register} isLoading={isLoading} /> : <Login />}</>;
 };
 
 export default Auth;
